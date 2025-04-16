f84b6e5657f090dd5ef4283d462ad584fda0e290
nehal <nehal@promactinfo.com>
2025-04-16 13:26:10 +0530

    the simple "Hello World" changes.

--- app/index.php
+++ app/index.php
@@ -1,7 +1,7 @@
 <?php
 $redis = new Redis();
 $redis->connect($_ENV['REDIS_HOST'], $_ENV['REDIS_PORT']);
-$redis->set("hello", "world");
+$redis->set("hello world!", "world");
 
 $db = new PDO(
     "pgsql:host=".$_ENV['DB_HOST'].";dbname=".$_ENV['DB_NAME'],
@@ -10,6 +10,6 @@ $db = new PDO(
 );
 
 echo "<h1>PHP App Running</h1>";
-echo "<p>Redis says: " . $redis->get("hello") . "</p>";
+echo "<p>Redis says: " . $redis->get("hello world!") . "</p>";
 echo "<p>Database connected successfully.</p>";
 ?>
